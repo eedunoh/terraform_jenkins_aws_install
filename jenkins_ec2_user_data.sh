a4a0a8a348a2f6e111800cb25a81280f07bfc32e
eedunoh <63975169+eedunoh@users.noreply.github.com>
2025-03-14 13:56:50 +0100

    made changes to user_data.sh

--- jenkins_ec2_user_data.sh
+++ jenkins_ec2_user_data.sh
@@ -21,15 +21,10 @@ sudo usermod -aG docker ec2-user
 docker --version
 
 
-# Install Docker Compose: Docker Compose v2 (binary plugin)
+# Install Docker Compose:
 
-# sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# sudo chmod +x /usr/local/bin/docker-compose
-
-DOCKER_COMPOSE_VERSION="v2.24.1"
-sudo mkdir -p /usr/local/lib/docker/cli-plugins
-sudo curl -SL https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
-sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 
 # Install libxcrypt-compat (Amazon Linux 2 requirement)
